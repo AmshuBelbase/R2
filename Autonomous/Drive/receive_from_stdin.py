d2783df5683600fc17d3449c3bb799afbb7ff067
Amshu Belbase <amsubelbs@gmail.com>
2024-04-06 00:48:51 +0530

    send receive data

---
+++ Autonomous/Drive/receive_from_stdin.py
@@ -0,0 +1,36 @@
+# Pico
+import sys
+import time
+import _thread
+from machine import Pin
+
+led = Pin(25, Pin.OUT)
+
+data = []
+
+
+def Core0():
+    global data
+    while True:
+        buf = sys.stdin.readline().rstrip('\n')
+        try:
+            data = [int(i) for i in buf.split('|')]
+            if (data[0] % 1000 == 0):
+                led.on()
+            else:
+                led.off()
+        except ValueError:
+            print("Non-integer detected.")
+            continue
+
+
+def Core1():
+    global data
+    while True:
+        print("received data:", data)
+        time.sleep(2)
+
+
+_thread.start_new_thread(Core1, ())
+
+Core0()
--- Autonomous/Drive/send_val_drive_to_pico.py
+++ Autonomous/Drive/send_val_drive_to_pico.py
@@ -2,25 +2,21 @@ import serial
 import time
 import random
 
-serial_port = '/dev/ttyACM0'
-baud_rate = 115200
-ser = serial.Serial(serial_port, parity=serial.PARITY_EVEN,
-                    stopbits=serial.STOPBITS_ONE, baudrate=baud_rate, timeout = 0.1)
+s = serial.Serial(port="/dev/ttyACM0", parity=serial.PARITY_EVEN,
+                  stopbits=serial.STOPBITS_ONE, timeout=1)
 
 
 class trying:
     def _init_(self):
         while True:
-            trying.call_back(ser)
+            trying.call_back(s)
 
     @staticmethod
     def call_back(ser):
         ser.flush()
-        # Open serial port
-        # time.sleep(0.5)
         at = time.time()
         print("Start", at)
-        # Define floats to send
+
         float1 = random.randint(0, 255)
         float2 = random.randint(0, 255)
         float3 = random.randint(0, 255)
@@ -37,6 +33,7 @@ class trying:
         print(f"Sent: {data}")
         print("End", time.time())
         print("Time taken", time.time() - at)
+        time.sleep(0.005)
 
 
 if __name__ == "__main__":
