9df888444a6c660c50be122d8825a3d9e3adc02b
Amshu Belbase <amsubelbs@gmail.com>
2024-04-08 15:38:51 +0530

    r2 working with flysky and uart

--- Manual/Mechanisms/Stepper.py
+++ Manual/Mechanisms/Stepper.py
@@ -3,32 +3,24 @@ from time import sleep_ms
 
 
 class StepperMotor:
-    def __init__(self, step_pin1, dir_pin1, step_pin2, dir_pin2):
+    def __init__(self, step_pin1, dir_pin1):
         self.step1 = Pin(step_pin1, Pin.OUT)
-        self.dir1 = Pin(dir_pin1, Pin.OUT)
-        self.step2 = Pin(step_pin2, Pin.OUT)
-        self.dir2 = Pin(dir_pin2, Pin.OUT)
+        self.dir1 = Pin(dir_pin1, Pin.OUT) 
 
     def stepper_up(self, steps):
-        self.dir1.off()
-        self.dir2.off()
+        self.dir1.off() 
 
         for _ in range(steps):
-            self.step1.on()
-            self.step2.on()
+            self.step1.on() 
             sleep_ms(1)
-            self.step1.off()
-            self.step2.off()
+            self.step1.off() 
             sleep_ms(1)
 
     def stepper_down(self, steps):
-        self.dir1.on()
-        self.dir2.on()
+        self.dir1.on() 
 
         for _ in range(steps):
-            self.step1.on()
-            self.step2.on()
+            self.step1.on() 
             sleep_ms(1)
-            self.step1.off()
-            self.step2.off()
+            self.step1.off() 
             sleep_ms(1)
--- Manual/Mechanisms/mechanisms_uart.py
+++ Manual/Mechanisms/mechanisms_uart.py
@@ -6,7 +6,7 @@ import time
 import utime
 
 
-uart = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))  
+uart = UART(1, baudrate=115200, tx=Pin(8), rx=Pin(9))  
 
 # roller servo
 roller_servo1_pin = 19
@@ -23,10 +23,9 @@ roller_pin1 = Pin(27, Pin.OUT)
 roller_pin2 = Pin(28, Pin.OUT)
 
 # elevator steppers
-elevator_step1_pin = 13
-elevator_dir1_pin = 12
-elevator_step2_pin = 15
-elevator_dir2_pin = 14
+elevator_step1_pin = 11
+elevator_dir1_pin = 10
+
  
 
 # elevator ultrasonic
@@ -40,8 +39,7 @@ roller_servo_motor1 = Servo(roller_servo1_pin)
 roller_servo_motor2 = Servo(roller_servo2_pin)
 gate_servo = Servo(gate_servo_pin)
 push_servo = Servo(push_servo_pin)
-stepper_motor = StepperMotor(
-    elevator_step1_pin, elevator_dir1_pin, elevator_step2_pin, elevator_dir2_pin)
+stepper_motor = StepperMotor(elevator_step1_pin, elevator_dir1_pin)
 steps = 0
 x_deg = 900
 y_deg = 1024 - x_deg
@@ -81,11 +79,11 @@ while True:
             
         if(step_up_down < -80 and stepper_up == 0):
             print("step_up")
-            stepper_motor.stepper_up(4000)
+            stepper_motor.stepper_up(8000)
             stepper_up = 1
         elif(step_up_down > 80 and stepper_up == 1):
             print("step_down")
-            stepper_motor.stepper_down(4000)
+            stepper_motor.stepper_down(8000)
             stepper_up = 0
             
         if(push_mech > 50):
