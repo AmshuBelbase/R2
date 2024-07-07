bdabbfba0db82253b093560ea64182ce17d65e3b
Amshu Belbase <amsubelbs@gmail.com>
2024-07-07 23:00:00 +0530

    mechanisms

--- Autonomous/Mechanisms/mechanisms_main.py
+++ Autonomous/Mechanisms/mechanisms_main.py
@@ -6,6 +6,16 @@ import utime
 
 # ----------------- PINOUTS -----------------
 
+i=0
+while i<5:
+    pass_val = 0
+    message = f"{pass_val}\n" 
+    # Send the message over USB CDC (print to USB)
+    print(message, end='')
+    i+=1
+    
+time.sleep(2)
+
 uart = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))  
 
 # roller servo
@@ -346,6 +356,7 @@ while True:
 #                 push_servo2.goto(300)
         
          
+
 
 
 
